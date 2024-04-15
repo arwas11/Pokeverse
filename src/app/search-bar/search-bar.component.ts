79064c739cafe5ca9197bf39f0e284b7022d6cc5
Arwa Sharif <arwas@google.com>
2024-04-15 11:29:20 -0500

    added a search bar

--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { Pokemon } from '../pokemon';
 
 @Component({
   selector: 'app-search-bar',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, FormsModule, MatFormFieldModule, MatInputModule],
   templateUrl: './search-bar.component.html',
   styleUrl: './search-bar.component.css'
 })
 export class SearchBarComponent {
+  searchedPokemon : Pokemon[] = []
+  searchInput = new FormControl('');
 
 }
